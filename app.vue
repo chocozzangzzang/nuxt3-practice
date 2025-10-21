9c9bac1772841124b51d0aac033dcfb4fe461cd7
chocozzangzzang <jangjh_05@naver.com>
2025-10-21 14:32:52 +0900

    NuxtApp/Plugin&Util/Auto-Import/Rendering/Hydration

--- app.vue
+++ app.vue
@@ -4,4 +4,15 @@
   </NuxtLayout>
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+console.log('nuxt app : ', nuxtApp);
+
+// ex. vue application instance
+// nuxtApp.vueApp.component()
+// nuxtApp.vueApp.directive()
+// nuxtApp.vueApp.use();
+
+// provide
+// nuxtApp.provide('hello', (msg: string) => `hello ${msg}`);
+</script>
--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -42,54 +42,58 @@
           />
         </div>
       </div>
-      <p class="q-mt-lg text-grey-8">
-        {{ course?.content }}
-        <q-separator class="q-mb-lg" />
-        <q-form class="q-gutter-y-md">
-          <q-btn
-            label="수강완료"
-            class="full-width"
-            color="green"
-            unelevated
-            :outline="completed ? false : true"
-            :icon="completed ? 'check' : undefined"
-            @click="completed = !completed"
-          />
-          <q-input
-            v-model="memo"
-            type="textarea"
-            outlined
-            dense
-            placeholder="메모를 작성해주세요."
-            rows="3"
-            autogrow
-          />
-        </q-form>
-      </p>
+      <ClientOnly>
+        <p class="q-mt-lg text-grey-8">
+          {{ course?.content }}
+          <q-separator class="q-mb-lg" />
+          <q-form class="q-gutter-y-md">
+            <q-btn
+              label="수강완료"
+              class="full-width"
+              color="green"
+              unelevated
+              :outline="completed ? false : true"
+              :icon="completed ? 'check' : undefined"
+              @click="completed = !completed"
+            />
+
+            <q-input
+              v-model="memo"
+              type="textarea"
+              outlined
+              dense
+              placeholder="메모를 작성해주세요."
+              rows="3"
+              autogrow
+            />
+          </q-form>
+        </p>
+      </ClientOnly>
+
       <template #footer>
+        <q-btn
+          v-if="prevCourse"
+          label="이전 강의"
+          color="primary"
+          unelevated
+          @click="movePage(prevCourse.path)"
+        />
         <ClientOnly>
-          <q-btn
-            v-if="prevCourse"
-            label="이전 강의"
-            color="primary"
-            unelevated
-            @click="movePage(prevCourse.path)"
-          />
           <q-btn
             label="쿼리 추가"
             color="dark"
             unelevated
             :to="{ path: $route.path, query: { timestamp: Date.now() } }"
           />
-          <q-space />
-          <q-btn
-            v-if="nextCourse"
-            label="다음 강의"
-            color="primary"
-            unelevated
-            @click="movePage(nextCourse.path)"
-          />
         </ClientOnly>
+        <q-space />
+        <q-btn
+          v-if="nextCourse"
+          label="다음 강의"
+          color="primary"
+          unelevated
+          @click="movePage(nextCourse.path)"
+        />
       </template>
     </AppCard>
   </div>
