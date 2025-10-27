085989a0ed0b8ad19cae7da060f515e4343402dc
chocozzangzzang <jangjh_05@naver.com>
2025-10-27 13:28:21 +0900

    auth api

---
+++ server/routes/auth/login.post.ts
@@ -0,0 +1,42 @@
+// auth/login post
+
+import { getUserByEmail } from '~/server/model/user';
+
+export default defineEventHandler(async (event) => {
+  const body = readBody<{ email: string; password: string }>(event);
+
+  const { email, password } = await body;
+
+  if (!email || !password) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Email address and Password are required',
+    });
+  }
+
+  const userWithPassword = getUserByEmail(email);
+
+  if (!userWithPassword) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Bad credentials',
+    });
+  }
+
+  const verified = verifyPassword(password, userWithPassword.password);
+  if (!verified) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'Bad credentials',
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { password: _password, ...userWithoutPassword } = userWithPassword;
+
+  setCookie(event, '__user', JSON.stringify(userWithoutPassword));
+
+  return {
+    user: userWithoutPassword,
+  };
+});
